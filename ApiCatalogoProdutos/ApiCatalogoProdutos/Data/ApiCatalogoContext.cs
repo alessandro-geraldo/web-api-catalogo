f2155d7cbcb91b6773df9be74eb3187535e1d492
alessandro geraldo <alessandro.com@hotmail.com>
2024-02-26 22:08:34 -0300

    commit inicial criacao do projeto

---
+++ ApiCatalogoProdutos/ApiCatalogoProdutos/Data/ApiCatalogoContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiCatalogoProdutos.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogoProdutos.Data
+{
+    public class ApiCatalogoContext : DbContext
+    {
+        public ApiCatalogoContext(DbContextOptions<ApiCatalogoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto>? Produtos { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }
+    }
+}
